eab5f1f020bb34058033914deb918489fc27ded7
nijasalih <nijasalih@gmail.com>
2023-01-18 12:36:41 +0530

    UI restructered

---
+++ lib/presentation/seller/screens/seller_home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class SellerHomePage extends StatefulWidget {
+  const SellerHomePage({super.key});
+
+  @override
+  State<SellerHomePage> createState() => _SellerHomePageState();
+}
+
+class _SellerHomePageState extends State<SellerHomePage> {
+  // final pages = [
+  // ];
+
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          title: const Text(
+            'DirecTrade',
+            style: TextStyle(
+              fontSize: 27,
+            ),
+          ),
+          actions: [
+            IconButton(
+              iconSize: 35,
+              onPressed: () {
+                Navigator.pushNamed(context, 'custprofile');
+              },
+              icon: const Icon(
+                Icons.account_circle_outlined,
+              ),
+            ),
+          ],
+        ),
+        // body: pages[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 35),
+              label: 'My Products', 
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart, size: 35),
+              label: 'My Orders',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat, size: 35),
+              label: 'Chat',
+            )
+          ],
+          // currentIndex: selectedIndex,
+          // onTap: (value) {
+          //   setState(() {
+          //     selectedIndex = value;
+          //   });
+          // },
+        ),
+      ),
+    );
+  }
+}
