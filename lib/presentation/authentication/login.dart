4d49c911b8500b62b7e282f592bb0d29b537f2d8
Hisham Chemmadan <112319214+C-hisham@users.noreply.github.com>
2023-01-24 14:37:53 +0530

    updated snackbar

--- lib/presentation/authentication/login.dart
+++ lib/presentation/authentication/login.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_project/presentation/user_model.dart';
@@ -96,8 +97,11 @@ class LoginPage extends StatelessWidget {
                           width: double.infinity,
                           height: 60.0,
                           child: TextButton(
+                            style: ButtonStyle(
+                              overlayColor: MaterialStateProperty.all(Colors.transparent)
+                            ),
                             onPressed: () async {
-                              if(kemail.text==null||kpass.text==null){
+                              if(kemail.text.isEmpty||kpass.text.isEmpty){
                                 ScaffoldMessenger.of(context).showSnackBar(
                                       const SnackBar(
                                         content: customsnackbar(errortext: 'Please enter your email/password',),
@@ -120,8 +124,16 @@ class LoginPage extends StatelessWidget {
                                       Navigator.pushNamed(
                                           context, 'sellerhome');
                                     } else if (users[i]['type'] == 'p') {
-                                      Navigator.pushNamed(
-                                          context, 'promptpending');
+                                      // Navigator.pushNamed(
+                                      //     context, 'promptpending');
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: yellowSnackBar(errortext: 'You are waiting to be approved',),
+                                        elevation: 0,
+                                        behavior: SnackBarBehavior.floating,
+                                        backgroundColor: Colors.transparent,
+                                      ),
+                                    );
                                     } else if (users[i]['type'] == 'c') {
                                       Navigator.pushNamed(context, 'home');
                                     } else if (users[i]['type'] == 'a') {
@@ -288,10 +300,11 @@ class customsnackbar extends StatelessWidget {
                       Radius.circular(20),
                 ),
                 child: IconButton(splashColor: Colors.transparent,onPressed:() {
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
                   
                 }, icon: Stack(alignment:Alignment.center,
                   children: [CircleAvatar(
-                    backgroundColor: Colors.red.shade500,radius: 40,
+                    backgroundColor: Colors.cyan.shade900,radius: 40,
                   ),
                     Icon(
                       Icons.cancel,color: Colors.black,
@@ -302,3 +315,92 @@ class customsnackbar extends StatelessWidget {
     );
   }
 }
+
+
+
+class yellowSnackBar extends StatelessWidget {
+  const yellowSnackBar({
+    Key? key, required this.errortext,
+  }) : super(key: key);
+
+  final String errortext;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: 90,
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              borderRadius:
+                  const BorderRadius.all(
+                      Radius.circular(15)),
+              color: Colors.amber.shade300),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 48,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment
+                          .start,
+                  children:  [
+                     Text('Please Wait!'.toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'roboto',
+                          fontWeight: FontWeight.bold,
+                          color:
+                              Colors.white,
+                        )),
+                        const Spacer(),
+                    Text(
+                      errortext,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow
+                          .ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+         Positioned(
+            top: -20,
+            left: -20,
+            child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.only(
+                  bottomLeft:
+                      Radius.circular(20),
+                ),
+                child: IconButton(splashColor: Colors.transparent,onPressed:() {
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                }, icon: Stack(alignment:Alignment.center,
+                  children: [CircleAvatar(
+                    backgroundColor: Colors.amber.shade700,radius: 40,
+                  ),
+                    Icon(
+                      Icons.cancel,color: Colors.black,
+                      size: 30,),
+                  ],
+                )))),
+      ],
+    );
+  }
+}
+
+
+
+
+
