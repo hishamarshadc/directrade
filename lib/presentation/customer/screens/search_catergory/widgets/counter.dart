70baa368947fa2a12afd0697281e043ba4428699
nijasalih <nijasalih@gmail.com>
2023-02-18 00:27:35 +0530

    product full view page and order placed

---
+++ lib/presentation/customer/screens/search_catergory/widgets/counter.dart
@@ -0,0 +1,26 @@
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+// class QCounter extends StatefulWidget {
+//   final int minValue;
+//   final int maxValue;
+//   const QCounter({super.key,required this.minValue, required this.maxValue});
+
+//   @override
+//   State<QCounter> createState() => _QCounterState();
+// }
+
+// class _QCounterState extends State<QCounter> {
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//   }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
