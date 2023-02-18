045ac512330db5724e8f46fd9efe8a24a2f30abe
nijasalih <nijasalih@gmail.com>
2023-02-18 10:40:07 +0530

    order list of customer updated

--- lib/presentation/customer/screens/order/orders_list.dart
+++ lib/presentation/customer/screens/order/orders_list.dart
@@ -1,30 +1,102 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_project/presentation/customer/screens/order/widgets/cust_order_card.dart';
 
 class OrderListPage extends StatelessWidget {
   const OrderListPage({super.key});
+  
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CustOrderCard(index:index),
-            ), itemCount: 20 ),
-      ),
+    final user=FirebaseAuth.instance.currentUser;
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection("Orders")
+          .where('customer_id', isEqualTo: user?.uid )
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(color: Colors.blue),
+            );
+          default:
+            if (snapshot.data!.docs.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 15, left: 10, right: 10, top: 15),
+                child: ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot orderdoc = snapshot.data!.docs[index];
+                      return StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('Products')
+                              .doc(orderdoc['product_id'])
+                              .snapshots(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
+
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                    color: Colors.blue),
+                              );
+                            }
+
+                            if (!snapshot.hasData) {
+                              return Text('Document does not exist');
+                            }
+                            // Extract data from the snapshot and display it
+                            final productdoc = snapshot.data!;
+
+                            return CustOrderCard(orderdoc: orderdoc,productdoc: productdoc);
+                          });
+                    }),
+              );
+            } else {
+              return Center(
+                child: const Text(
+                  'No Orders',
+                ),
+              );
+            }
+        }
+      },
     );
+
+    // Padding(
+    //                     padding: const EdgeInsets.all(8.0),
+    //                     child: CustOrderCard(orderdocument: document),
+    //                   );
+
+    // Scaffold(
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(8.0),
+    //     child: ListView.builder(
+    //         itemBuilder: (context, index) => Padding(
+    //           padding: const EdgeInsets.all(8.0),
+    //           child: CustOrderCard(index:index),
+    //         ), itemCount: 20 ),
+    //   ),
+    // );
   }
 }
 
-class Order {
-  final String? productName;
-  final double? quantity;
-  final double? totalPrice;
-  final String? statusText;
+// class Order {
+//   final String? productName;
+//   final double? quantity;
+//   final double? totalPrice;
+//   final String? statusText;
 
-  Order({this.productName, this.quantity, this.totalPrice, this.statusText});
-}
+//   Order({this.productName, this.quantity, this.totalPrice, this.statusText});
+// }
